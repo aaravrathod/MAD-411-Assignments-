c6f41479f0e1647a1d0c64a91ef5d9eb7cbc6df2
aaravrathod <W0838110@myscc.ca>
2025-04-02 16:09:20 -0400

    #1 Started working on assignment 10. Made the layout and added properties to my DataClass

--- app/src/main/java/com/example/assignment6aaravrathid/DataClass.kt
+++ app/src/main/java/com/example/assignment6aaravrathid/DataClass.kt
@@ -1,6 +1,7 @@
 package com.example.assignment6aaravrathid
 
 import java.io.Serializable
+import java.util.Currency
 
 data class DataClass ( val name:String, val amount:String,  val date:String):Serializable
 
